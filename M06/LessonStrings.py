eaf80cb9a185510fc3133d8e9c17dfec4b1f4d88
Darrell Gerber <darrellgerber@gmail.com>
2021-12-20 17:56:03 -0600

    After crash

---
+++ M06/LessonStrings.py
@@ -0,0 +1,6 @@
+# Using the other quote for the nested quote
+string3 = "'Always look on the bright side of life, ' he said."
+# Using escaped quotes for the nested quote
+string4 = "\"Always look on the bright side of life,\" he said."
+print(string3)
+string4
